ef557066a4186a011b8e2aca309f91eb560a790f
Guilherme Araújo <arauujogui@gmail.com>
2022-08-01 09:16:08 -0300

    Set up Inter font family

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
+import theme from "../lib/theme";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
