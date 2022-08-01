e96fc3fc26d50947933ef5b394ed389e184aa6e9
Guilherme Araújo <arauujogui@gmail.com>
2022-08-01 09:29:42 -0300

    Add homepage title

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import About from "../components/About";
 import Foreword from "../components/Foreword";
 
 const Index = () => {
   return (
     <main>
+      <Head>
+        <title>Guilherme Araújo</title>
+      </Head>
       <Foreword />
       <About />
     </main>
